40cfeba060aeb0ab3e3edb0d2a5b68bc2fd34714
Anna Andrade <aandra12@jh.edu>
2025-04-03 20:52:03 -0400

    saving progress here cuz i'm going to redo it

--- OOTD/app/src/main/java/com/example/ootd/LoginActivity.java
+++ OOTD/app/src/main/java/com/example/ootd/LoginActivity.java
@@ -7,10 +7,13 @@ import android.view.View;
 import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
 import androidx.viewpager2.widget.ViewPager2;
 
 import com.google.android.material.tabs.TabLayout;
@@ -32,25 +35,41 @@ public class LoginActivity extends AppCompatActivity {
         stinkerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                // close the login page
+                // login
                 Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                 startActivity(intent);
                 finish();
             }
         });
 
-        /*
+
         TabLayout loginTabLayout = findViewById(R.id.loginTabs);
         ViewPager2 viewPager = findViewById(R.id.loginViewPager);
 
+        viewPager.setAdapter(new FragmentStateAdapter(this) {
+            @NonNull
+            @Override
+            public Fragment createFragment(int position) {
+                if (position == 0) {
+                    return new LoginFragment();
+                } else {
+                    return new NewUserFragment();
+                }
+            }
+
+            @Override
+            public int getItemCount() {
+                return 2;
+            }
+        });
+
         new TabLayoutMediator(loginTabLayout, viewPager, (tab, position) -> {
             if (position == 0) {
-                tab.setText("Login");
+                tab.setText("bruh?");
             } else {
                 tab.setText("New User");
             }
         }).attach();
-         */
+
     }
 }
