ac43f4a6af672ea5a4a77dea90bee66691312e2e
emguan <eguan3@jh.edu>
2025-04-06 20:04:19 -0400

    functioning image displays

--- OOTD/app/src/main/java/com/example/ootd/AddItemFragment.java
+++ OOTD/app/src/main/java/com/example/ootd/AddItemFragment.java
@@ -7,10 +7,13 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -30,12 +33,6 @@ import com.google.firebase.storage.UploadTask;
 import java.io.ByteArrayOutputStream;
 import android.net.Uri;
 
-
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link AddItemFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class AddItemFragment extends Fragment {
 
     private FragmentAddItemBinding binding;
@@ -45,44 +42,13 @@ public class AddItemFragment extends Fragment {
     Button saveButton;
     private static final int CAMERA_CODE = 100;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
     public AddItemFragment() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AddItemFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AddItemFragment newInstance(String param1, String param2) {
-        AddItemFragment fragment = new AddItemFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -94,49 +60,26 @@ public class AddItemFragment extends Fragment {
         takePicture = binding.imageButton;
         imageView = binding.imageDisplay;
 
-        takePicture.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(cameraIntent, CAMERA_CODE);
-            }
+        takePicture.setOnClickListener(v -> {
+            Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            startActivityForResult(cameraIntent, CAMERA_CODE);
         });
         return root;
     }
 
-
     @Override
     public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        if (requestCode == CAMERA_CODE && resultCode == RESULT_OK) {
+        if (requestCode == CAMERA_CODE && resultCode == RESULT_OK && data != null && data.getExtras() != null) {
             Bitmap photo = (Bitmap) data.getExtras().get("data");
             imageView.setImageBitmap(photo);
-
-            // once the user takes the picture, display a button to save
             saveButton = binding.addItemSubmitButton;
             saveButton.setVisibility(View.VISIBLE);
-
-            // save the photo to the database
-            saveButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    uploadImageToFirebaseStorage(photo);
-
-                    Navigation.findNavController(v).navigate(R.id.navigation_garment_listing);
-
-
-                    //send back to closet view
-                    //yoinking this for later
-                    //NavController navController = Navigation.findNavController(getActivity(), R.id.nav_host_fragment_activity_main);
-                    //navController.navigate(R.id.action_addItemFragment_to_closetFragment);
-                }
-            });
-
+            saveButton.setOnClickListener(v -> uploadImageToFirebaseStorage(photo));
         } else {
             Toast.makeText(getContext(), "Cancelled", Toast.LENGTH_SHORT).show();
             super.onActivityResult(requestCode, resultCode, data);
         }
     }
-
 
     public byte[] convertBitmapToByteArray(Bitmap bitmap) {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
@@ -144,40 +87,32 @@ public class AddItemFragment extends Fragment {
         return baos.toByteArray();
     }
 
-
     private void uploadImageToFirebaseStorage(Bitmap bitmap) {
         FirebaseStorage storage = FirebaseStorage.getInstance();
         StorageReference storageRef = storage.getReference();
-        StorageReference imageRef = storageRef.child("images/" + System.currentTimeMillis() + ".jpg");
+        String path = "images/" + System.currentTimeMillis() + ".jpg";
+        StorageReference imageRef = storageRef.child(path);
 
         byte[] data = convertBitmapToByteArray(bitmap);
 
         UploadTask uploadTask = imageRef.putBytes(data);
-        uploadTask.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-            @Override
-            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                Toast.makeText(getActivity(), "Upload successful!", Toast.LENGTH_SHORT).show();
-                imageRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
-                    @Override
-                    public void onSuccess(Uri uri) {
-                        Bundle bundle = new Bundle();
-                        String downloadUrl = uri.toString();
+        uploadTask.addOnSuccessListener(taskSnapshot -> {
+            Toast.makeText(getActivity(), "Upload successful!", Toast.LENGTH_SHORT).show();
+            imageRef.getDownloadUrl().addOnSuccessListener(uri -> {
+                if (uri != null) {
+                    String downloadUrl = uri.toString();
+                    Bundle bundle = new Bundle();
+                    bundle.putString("ImagePath", path);
 
-                        bundle.putString("ImageURL", downloadUrl);
+                    GarmentListingFragment fragmentB = new GarmentListingFragment();
+                    fragmentB.setArguments(bundle);
 
-
-                        GarmentListingFragment fragmentB = new GarmentListingFragment();
-                        fragmentB.setArguments(bundle);
-                    }
-                });
-            }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                Toast.makeText(getActivity(), "Upload failed: " + e.getMessage(), Toast.LENGTH_SHORT).show();
-            }
-        });
+                    NavController navController = Navigation.findNavController(getActivity(), R.id.nav_host_fragment_activity_main); // Adjust the ID as per your layout
+                    navController.navigate(R.id.action_addItemFragment_to_garmentListingFragment, bundle);
+                } else {
+                    Log.d("Debug", "URI is null");
+                }
+            });
+        }).addOnFailureListener(e -> Toast.makeText(getActivity(), "Upload failed: " + e.getMessage(), Toast.LENGTH_SHORT).show());
     }
-
 }
-
--- OOTD/app/src/main/java/com/example/ootd/GarmentListingFragment.java
+++ OOTD/app/src/main/java/com/example/ootd/GarmentListingFragment.java
@@ -1,20 +1,27 @@
 package com.example.ootd;
 
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.example.ootd.databinding.FragmentGarmentListingBinding;
 import com.example.ootd.databinding.FragmentProfileBinding;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.textfield.MaterialAutoCompleteTextView;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -29,8 +36,10 @@ public class GarmentListingFragment extends Fragment {
 
     private FragmentGarmentListingBinding binding;
     Button saveButton;
+    FirebaseStorage storage;
     FirebaseDatabase database;
-    DatabaseReference myRef;
+    StorageReference myRef;
+    DatabaseReference dbref;
 
 
     public GarmentListingFragment() {
@@ -41,49 +50,6 @@ public class GarmentListingFragment extends Fragment {
                              Bundle savedInstanceState) {
         binding = FragmentGarmentListingBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
-        setupDropdownMenu_Category();
-        setupDropdownMenu_SubCategory();
-        database = FirebaseDatabase.getInstance();
-        myRef = database.getReference("Garment");
-
-
-        saveButton = binding.SaveListing;
-
-        saveButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                String category = binding.Tops.getText().toString();
-                String subcategory = binding.Blouse.getText().toString();
-                if (category.isEmpty()) {
-                    Toast.makeText(getActivity(), "Category Required", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-
-                if (subcategory.isEmpty()) {
-                    Toast.makeText(getActivity(),"Subcategory Required",Toast.LENGTH_SHORT).show();
-                    return;
-                }
-
-                Bundle bundle = getArguments();
-
-                String userId = myRef.push().getKey();
-                Map<String, Object> userData = new HashMap<>();
-
-                if (bundle != null) {
-                    String URL = bundle.getString("ImageURL");
-                    userData.put("URL", URL);
-                }
-
-                userData.put("Category", category);
-                userData.put("Subcategory", subcategory);
-
-                myRef.child(userId).setValue(userData);
-
-                NavController navController = Navigation.findNavController(getActivity(), R.id.nav_host_fragment_activity_main);
-                navController.navigate(R.id.navigation_closet);
-            }
-        });
 
         return root;
     }
@@ -101,6 +67,70 @@ public class GarmentListingFragment extends Fragment {
             autoCompleteTextView.setText(adapter.getItem(i));
         });
     }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        storage = FirebaseStorage.getInstance();
+        myRef = storage.getReference();
+        saveButton = binding.SaveListing;
+
+        setupDropdownMenu_Category();
+        setupDropdownMenu_SubCategory();
+
+        Bundle bundle = getArguments();
+        String path = bundle.getString("ImagePath");
+        StorageReference imageRef = myRef.child(path);
+
+        ImageView imageView = binding.ListingImage;
+        imageRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                Log.d("LoadImage", "URL: " + uri.toString()); // Log the URL to check it's correct
+                Glide.with(getActivity())
+                        .load(uri)
+                        .into(imageView);
+                Toast.makeText(getActivity(),"Image successfully captured",Toast.LENGTH_SHORT).show();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                Log.e("LoadImage", "Oop" + path, exception); // Log the exception
+                Toast.makeText(getActivity(),"Failed to Retrieve Image",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String category = binding.Tops.getText().toString();
+                String subcategory = binding.Blouse.getText().toString();
+                if (category.isEmpty()) {
+                    Toast.makeText(getActivity(), "Category Required", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (subcategory.isEmpty()) {
+                    Toast.makeText(getActivity(),"Subcategory Required",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                String userId = dbref.push().getKey();
+                Map<String, Object> userData = new HashMap<>();
+
+                userData.put("ImagePath", path);
+                userData.put("Category", category);
+                userData.put("Subcategory", subcategory);
+
+                dbref.child(userId).setValue(userData);
+
+                NavController navController = Navigation.findNavController(getActivity(), R.id.nav_host_fragment_activity_main);
+                navController.navigate(R.id.navigation_closet);
+            }
+        });
+    }
+
     public void setupDropdownMenu_SubCategory() {
         String[] items = new String[]{"Blouse"};
         ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(),
